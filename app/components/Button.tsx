4f4dafdf85d26aa839defe8ca67d6dbe7960d3d9
Edwin Kesuma <edwin.kesuma.EK21@gmail.com>
2026-05-11 14:18:12 +0700

    refactor: add type, disabled, and onclick on custom button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
 type ButtonProps = {
-    isPrimary: boolean;
+    isPrimary?: boolean;
     children: React.ReactNode;
+    type?: "button" | "submit" | "reset";
+    disabled?: boolean;
+    onClick?: () => void;
 }
 
 
-const Button = ({isPrimary, children}: ButtonProps) => {
+const Button = ({
+                    isPrimary, children, type = "button", disabled = false, onClick,
+                }: ButtonProps) => {
     return (
-        <div className={`flex flex-row items-center justify-center gap-2 ${isPrimary?"bg-red-600":"bg-white"} ${isPrimary?"text-white":"text-red-600"} border border-red-600 px-10 py-3 rounded-md`}>
+        <button
+            type={type ?? ""}
+            disabled={disabled ?? false}
+            onClick={onClick}
+            className={`flex flex-row items-center justify-center gap-2 font-bold ${isPrimary ? "bg-red-600" : "bg-white"} ${isPrimary ? "text-white" : "text-red-600"} border border-red-600 px-10 py-3 rounded-md disabled:opacity-50`}>
             {children}
-        </div>
+        </button>
     );
 };
 
